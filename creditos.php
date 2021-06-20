81b86a793fae878f6d3db6fe0049d6fb63f6b388
SebastianMacielAvila <79530003+SebastianMacielAvila@users.noreply.github.com>
2021-06-20 17:25:13 -0500

    Integrantes del equipo desarrollador

--- creditos.php
+++ creditos.php
@@ -9,6 +9,7 @@
 		<h3>>Barrera Santiago Omar - Consultor, experto en BD - Brodie</h3>
 		<h3>>Maciel Ávila Sebastián - Desarrollador, experto en programación - Noob Saibot.</h3>
 		<h3>>Escogido Bravo Leonardo - Consultor - El Chavísimo.</h3>
+		<h3>>Mendoza Islas Alan - Documentador - El Wero.</h3>
 		<body background="DEMONS.jpeg">
 	</body>
 </html>
